265e02e9f827061e62c1a7fc17967d222489a844
Bill Morris <wboykinm@geosprocket.com>
2014-10-11 08:48:02 -0400

    images and svgs for the font

--- app/db.js
+++ app/db.js
@@ -2,5 +2,5 @@ var knex = require('knex');
 
 module.exports = knex({
   client: 'pg',
-  connection: 'postgres://tristan:@127.0.0.1/hackvt'
+  connection: 'postgres://wboykinm@127.0.0.1/hackvt'
 });
