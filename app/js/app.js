4a5f24f674216485c0e21bbab5f0a75e4c70d011
Tristan Davies <github@tristan.io>
2014-10-11 01:37:42 -0400

    aggregate all of the things

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['n3-line-chart'])
 
   .controller('main', function($scope, $http){
-    $scope.now = new Date('7/23/2012');
+    $scope.now = new Date('7/23/2012 12:10');
     setInterval(function(){
       var now = new Date($scope.now);
       now.setMinutes(now.getMinutes()+1);
@@ -39,13 +39,19 @@ angular.module('app', ['n3-line-chart'])
       },
       series: [{
         y: 'main',
-        thickness: '2px',
+        thickness: '3px',
         drawDots: false
       }]
     };
+    for (var key in $scope.plan) {
+      $scope.currentUsageOptions.series.push({
+        y: key,
+        thickness: '1.5px',
+        drawDots: false
+      });
+    }
 
     $scope.$watch('now', function(now){
-      console.log(5, now);
       if (now) {
         $http.get('/latest/'+now.valueOf()).then(function(stuff){
           var data = stuff.data;
@@ -56,4 +62,38 @@ angular.module('app', ['n3-line-chart'])
         });
       }
     });
+
+    $http.get('/today').then(function(stuff){
+      var data = stuff.data;
+      data.forEach(function(row){
+        row.date_time = new Date(row.date_time);
+      });
+      $scope.todayUsage = data.map(function(row){
+        row.average_hp /= 3;
+        return row;
+      });
+    });
+
+    $scope.anomaly = {
+      title: 'Heat pump',
+      chartOptions: {
+        lineMode: 'basis',
+        tooltip: {mode: "scrubber"},
+        axes: {
+          x: {
+            key: 'date_time',
+            type: 'date'
+          }
+        },
+        series: [{
+          y: 'hp',
+          thickness: '3px',
+          drawDots: false
+        }, {
+          y: 'average_hp',
+          thickness: '1px',
+          drawDots: false
+        }]
+      }
+    };
   });
--- app/server.js
+++ app/server.js
@@ -27,6 +27,100 @@ var latest = function(since){
     .map(formatRow);
 };
 
+var reduceToHours = function(rows){
+  var out = [];
+  rows.forEach(function(row){
+    var hour = row.date_time.getHours();
+    var outRow;
+    for (var i = 0; i < out.length; i++) {
+      if (out[i].hours == hour) {
+        outRow = out[i];
+        break;
+      }
+    }
+    if (!outRow) {
+      outRow = {
+        hours: hour,
+        rows: []
+      };
+      out.push(outRow);
+    }
+    outRow.rows.push(row);
+  });
+
+  var averages = out.map(function(outRow){
+    var newRow = {};
+    outRow.rows.forEach(function(row){
+      for (var key in row) {
+        if (key != 'id' && key != 'date_time') {
+          if (typeof newRow[key] == 'undefined') {
+            newRow[key] = 0;
+          }
+          newRow[key] += row[key];
+        }
+      }
+    });
+    for (var key in newRow) {
+      newRow[key] /= outRow.rows.length;
+    }
+
+    newRow.date_time = outRow.rows[0].date_time;
+
+    return newRow;
+  });
+
+  return averages;
+};
+
+var today = function(){
+  return db.select('*')
+    .from('account1')
+    .orderBy('date_time', 'desc')
+    .limit(60*24*7)
+    .map(formatRow)
+    .then(function(rows){
+      var today = reduceToHours(rows.slice(0, 60*24));
+
+      var sumDay = rows.slice(0, 60*24).map(function(row){
+        return [row];
+      });
+      for (var i = 1; i < 7; i++) {
+        var newDay = rows.slice(60*24*i, 60*24*i+1);
+        for (var i2 = 0; i2 < newDay.length; i2++) {
+          sumDay[i2].push(newDay[i2]);
+        }
+      }
+      var averageDay = sumDay.map(function(days){
+        var totalDays = days.length;
+        var day = days.reduce(function(a, b){
+          var out = {};
+          for (var key in a) {
+            if (key != 'id' && key != 'date_time') {
+              out[key] = a[key] + b[key];
+            }
+          }
+          return out;
+        });
+
+        for (var key in day) {
+          if (key != 'id' && key != 'date_time') {
+            day[key] /= totalDays;
+          }
+        }
+
+        return day;
+      });
+
+      today.forEach(function(day, i){
+        for (var key in averageDay[i]) {
+          day['average_'+key] = averageDay[i][key];
+        }
+      });
+
+      return today;
+    });
+};
+
 
 var app = express();
 
@@ -36,8 +130,17 @@ app.get('/latest/:since', function(req, res){
   latest(req.params.since).then(function(data){
     res.status(200).send(data);
   }).catch(function(err){
-    console.error('err!', err);
-    res.status(500, err);
+    console.error('err!', err.stack);
+    res.status(500, err.stack);
+  });
+});
+
+app.get('/today', function(req, res){
+  today().then(function(data){
+    res.status(200).send(data);
+  }).catch(function(err){
+    console.error('err!', err.stack);
+    res.status(500, err.stack);
   });
 });
 
